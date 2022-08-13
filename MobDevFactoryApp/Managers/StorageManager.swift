98bc98f4e8c20a34ea2f667cf29deca88b30129a
DimkaVinka <dimkavinka@gmail.com>
2022-08-13 10:50:16 +0300

    add storageManager file
    fix issues

---
+++ MobDevFactoryApp/Managers/StorageManager.swift
@@ -0,0 +1,40 @@
+//
+//  StorageManager.swift
+//  MobDevFactoryApp
+//
+//  Created by Дмитрий Виноградов on 13.08.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class User: Object {
+    @Persisted var nickName: String = ""
+    @Persisted var name: String = ""
+    @Persisted var surName: String = ""
+    @Persisted var password: String = ""
+}
+
+class StorageManager {
+    let realm = try! Realm()
+    var items: Results<User>!
+    
+    // MARK: - Обращание к хранилищу
+    func makeStorage() {
+        items = realm.objects(User.self)
+    }
+    
+    // MARK: - Добавление данных
+    func addUser(_ user: User) {
+        try! self.realm.write({
+            self.realm.add(user)
+        })
+    }
+    
+    // MARK: - Удаление данных
+    func deleteUser(_ user: User) {
+        try! self.realm.write {
+            self.realm.delete(user)
+        }
+    }
+}
