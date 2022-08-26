fc0a333c46e4e47afaa68586d6eab3e513b7c663
marivolvakova <104025325+marivolvakova@users.noreply.github.com>
2022-08-26 20:20:52 +0300

    Calendar view.

--- MobDevFactoryApp/Modules/TabBarViewControlers/CalendarViewController/CalendarViewController.swift
+++ MobDevFactoryApp/Modules/TabBarViewControlers/CalendarViewController/CalendarViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 import SnapKit
 
 final class CalendarViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    lazy var calendarView: UICalendarView = {
+        let calendarView = UICalendarView()
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        return calendarView
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        setupView()
+        setupHierarchy()
+        setupLayout()
+
+    }
+    
+    private func setupView() {
+        view.backgroundColor = Metric.colorBackround
+    }
+    private func setupHierarchy() {
+        view.addSubview(calendarView)
+    }
+    
+    private func setupLayout() {
+        calendarView.snp.makeConstraints { make in
+            make.left.equalTo(view).offset(10)
+            make.right.equalTo(view).offset(-10)
+            make.height.equalTo(300)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
+        }
     }
 }
