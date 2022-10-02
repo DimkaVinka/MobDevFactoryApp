3d53d085d93eec68c03185bc4839239e9bbae3b5
Fedor Donskov <fedya_donskov@mail.ru>
2022-10-02 15:20:20 +0300

    Added a controller, ViewModel. Added icons. Configured the tableView.

--- MobDevFactoryApp/Modules/TabBarModules/SettingsModule/View/SettingsViewController.swift
+++ MobDevFactoryApp/Modules/TabBarModules/SettingsModule/View/SettingsViewController.swift
@@ -6,11 +6,154 @@
 //
 
 import UIKit
+import SnapKit
+import Combine
 
 class SettingsViewController: UIViewController {
 
+    var viewModel = SettingsViewModel()
+    var sections: [Section]?
+    private var observer: AnyCancellable?
+
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(SettingTableCell.self,
+                       forCellReuseIdentifier: SettingTableCell.identifire)
+        table.register(SettingSwitchTableCell.self,
+                       forCellReuseIdentifier: SettingSwitchTableCell.switchIdentifire)
+        return table
+    }()
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "Настройки"
+
+        viewModel.createModel()
+
+        observer = viewModel.$sections.sink { value in
+            self.sections = value
+        }
+
+//        configureView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Private methods
+
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    private func setupLayout() {
+
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top)
+            make.leading.equalTo(view.snp.leading)
+            make.trailing.equalTo(view.snp.trailing)
+            make.bottom.equalTo(view.snp.bottom)
+        }
     }
 }
+
+// MARK: - UITableViewDelegate
+
+extension SettingsViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let type = sections?[indexPath.section].options[indexPath.row]
+
+        //        tableView.deselectRow(at: indexPath, animated: true)
+        //        let infoViewController = InfoViewController()
+        //        infoViewController = models[indexPath.section].options[indexPath.row]
+        //        navigationController?.pushViewController(infoViewController, animated: true)
+
+        switch type.self {
+        case .staticCell(let model):
+            print("Нажата ячейка \(model.title)")
+            model.handler()
+        case .switchCell(let model):
+            print("Нажата ячейка \(model.title)")
+            model.handler()
+        case .none:
+            print("Error")
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SettingsViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = sections?[section]
+        return section?.title
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        sections?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections?[section].options.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = sections?[indexPath.section].options[indexPath.row]
+
+        switch model.self {
+        case .staticCell(let model):
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingTableCell.identifire,
+                for: indexPath
+            ) as? SettingTableCell else {
+                return UITableViewCell()
+            }
+
+            cell.layer.cornerRadius = 20
+            cell.layer.masksToBounds = true
+
+            cell.configure(with: model)
+            return cell
+
+        case .switchCell(let model):
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingSwitchTableCell.switchIdentifire,
+                for: indexPath
+            ) as? SettingSwitchTableCell else {
+                return UITableViewCell()
+            }
+
+            cell.layer.cornerRadius = 20
+            cell.layer.masksToBounds = true
+
+            cell.configure(with: model)
+            return cell
+        case .none:
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: "cell",
+                for: indexPath
+            ) as? UITableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+            print("cellForRowAt Error")
+        }
+    }
+
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        tableView.deselectRow(at: indexPath, animated: true)
+    //        let infoViewController = InfoViewController()
+    //        navigationController?.pushViewController(infoViewController, animated: true)
+    //    }
+}
+
--- MobDevFactoryApp/Modules/TabBarModules/SettingsModule/ViewModel/SettingsViewModel.swift
+++ MobDevFactoryApp/Modules/TabBarModules/SettingsModule/ViewModel/SettingsViewModel.swift
@@ -6,11 +6,55 @@
 //
 
 import UIKit
+import Combine
 
+class SettingsViewModel {
 
-class SettingsViewModel: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .purple
+    @Published var sections = [Section]()
+
+    func createModel() {
+        sections = [
+            Section(title: "Основные", options: [
+                .staticCell(model: SettingsOption(title: "Аккаунт",
+                                                  icon: UIImage(image: .account),
+                                                  iconBackgroungColor: nil) {
+
+                                                  }),
+                .staticCell(model: SettingsOption(title: "Уведомления",
+                                                  icon: UIImage(image: .notification),
+                                                  iconBackgroungColor: nil) {
+
+                                                  }),
+                .staticCell(model: SettingsOption(title: "Конфиденциальность и безопасность",
+                                                  icon: UIImage(image: .privacySecurity),
+                                                  iconBackgroungColor: nil) {
+
+                                                  }),
+                .switchCell(model: SettingsSwitchOption(title: "Темный режим",
+                                                        icon: UIImage(image: .darkMode),
+                                                        iconBackgroungColor: nil,
+                                                        handler: {
+
+                                                        },
+                                                        isOn: false))
+            ]),
+
+            Section(title: "Дополнительные", options: [
+                .staticCell(model: SettingsOption(title: "Язык",
+                                                  icon: UIImage(image: .language),
+                                                  iconBackgroungColor: nil) {
+//                                                      SceneDelegate.shared.changeViewController(viewController: LanguageViewController(), animationOptions: .curveEaseOut)
+                                                  }),
+                .staticCell(model: SettingsOption(title: "Выйти",
+                                                  icon: UIImage(image: .quit),
+                                                  iconBackgroungColor: nil) {
+
+                                                  })
+            ])
+        ]
     }
 }
+
+
+
+
