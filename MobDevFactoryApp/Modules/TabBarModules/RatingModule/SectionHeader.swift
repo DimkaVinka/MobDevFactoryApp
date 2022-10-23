92fa9a792236486e7d3bc68a966c4b43e13f4d0a
marivolvakova <104025325+marivolvakova@users.noreply.github.com>
2022-10-24 00:30:32 +0300

    Rating module.

--- MobDevFactoryApp/Modules/TabBarModules/RatingModule/SectionHeader.swift
+++ MobDevFactoryApp/Modules/TabBarModules/RatingModule/SectionHeader.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 
-class SectionHeader: UICollectionReusableView {
-    
+class SectionHeader: UICollectionViewListCell {
+
     static let identifier = "SectionHeader"
     
+    var view: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+
     var imageIcon: UIImageView = {
         let image = UIImageView()
         image.contentMode = .scaleAspectFit
@@ -20,59 +26,51 @@ class SectionHeader: UICollectionReusableView {
         image.image = UIImage(systemName: "play.rectangle.fill")
         return image
     }()
-    
+
     var lableTitle: UILabel = {
         let lable = UILabel()
-        lable.font = UIFont.preferredFont(forTextStyle: .title3)
+        lable.font = UIFont.preferredFont(forTextStyle: .headline)
         lable.adjustsFontForContentSizeCategory = true
         lable.textColor = .white
         return lable
     }()
-    
-    var forwardImage: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(systemName: "chevron.down")
-        image.tintColor = .white
-        return image
-    }()
-    
+
     var lableNumber: UILabel = {
         let lable = UILabel()
-        lable.font = UIFont.preferredFont(forTextStyle: .title3)
+        lable.font = UIFont.preferredFont(forTextStyle: .headline)
         lable.adjustsFontForContentSizeCategory = true
         lable.textColor = .white
         return lable
     }()
     
+
     override init(frame: CGRect) {
-        super.init(frame: frame)
+      super.init(frame: frame)
         setupView()
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupView(){
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+    func setupView() {
+        
+        backgroundColor = UIColor(red: 18 / 255, green: 14 / 255, blue: 62 / 255, alpha: 1)
         addSubview(imageIcon)
         imageIcon.translatesAutoresizingMaskIntoConstraints = false
         imageIcon.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
         imageIcon.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-    
+
         addSubview(lableTitle)
-    
+
         lableTitle.translatesAutoresizingMaskIntoConstraints = false
         lableTitle.leadingAnchor.constraint(equalTo: imageIcon.trailingAnchor, constant: 17).isActive = true
         lableTitle.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-    
-        addSubview(forwardImage)
-        forwardImage.translatesAutoresizingMaskIntoConstraints = false
-        forwardImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
-        forwardImage.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-    
+
+
         addSubview(lableNumber)
         lableNumber.translatesAutoresizingMaskIntoConstraints = false
-        lableNumber.trailingAnchor.constraint(equalTo: forwardImage.trailingAnchor, constant: -30).isActive = true
+        lableNumber.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
         lableNumber.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
 }
--- MobDevFactoryApp/Modules/TabBarModules/RatingModule/Teams.swift
+++ MobDevFactoryApp/Modules/TabBarModules/RatingModule/Teams.swift
@@ -7,31 +7,49 @@
 
 import Foundation
 
-struct Teams: Codable {
+struct Teams: Codable, Hashable {
     let teams: [Team]
 }
 
-struct Team: Codable {
+struct Team: Codable, Hashable {
     let courators: [Courator]
     let teamScore: Int
     let teamName: String
     let team: [Student]
 }
 
-struct Students: Codable {
+struct Students: Codable, Hashable {
     let students: [Student]
 }
 
-struct Student: Codable {
+struct Student: Codable, Hashable {
     let name: String
     let lastname: String
     let score: Int
 }
 
-struct Courators: Codable {
+struct Courators: Codable, Hashable {
     let courators: [Courator]
 }
 
-struct Courator: Codable {
+struct Courator: Codable, Hashable {
     let name: String
+}
+
+
+enum ListItem: Hashable {
+    case header(Team)
+    case body(Student)
+}
+
+
+enum SectionCollection {
+    case main
+}
+
+
+enum Places: CaseIterable {
+    case first
+    case second
+    case third
 }
